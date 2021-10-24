d4e5e7da85a77fce3bfb186fc8d257d892a6e815
Vinicius de Lima Gomes <vinicius.gomes@sulamerica.com.br>
2021-10-23 21:59:59 -0300

    models produto e variacao - aula02

---
+++ utils/utils.py
@@ -0,0 +1,24 @@
+import os
+from django.conf import settings
+from PIL import Image
+
+
+def resize_image(img, new_width=800):
+    img_full_path = os.path.join(settings.MEDIA_ROOT, img.name)
+    img_pil = Image.open(img_full_path)
+    original_width, original_height = img_pil.size
+
+    if original_width <= new_width:
+        print('Retornando largura original menor/igual que nova largura')
+        img_pil.close()
+        return
+
+    new_height = round((new_width * original_height) / original_width)
+
+    new_img = img_pil.resize((new_width, new_height), Image.LANCZOS)
+    new_img.save(
+        img_full_path,
+        optimize=True,
+        quality=50
+    )
+    print('Imagem redimensionada.')
